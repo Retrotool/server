3926ee83165257f93785dbd314192de44dec08e6
Alexei Volkov <lifecoder.ua@gmail.com>
2016-10-02 22:27:04 +0300

    #1 feat(Cards): implement Cards scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
+  resources :cards
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
